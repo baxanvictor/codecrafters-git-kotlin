e7544486c3e08dfa84045ed2d1a7310697b56872
Victor Baxan <vbaxan.linkedin@gmail.com>
2026-05-30 10:25:09 +0300

    Fixed printing inside cat-file command. Everything passes now. Cleanup/refactoring next.

--- app/src/main/kotlin/processcommand/processCatFileCommand.kt
+++ app/src/main/kotlin/processcommand/processCatFileCommand.kt
@@ -1,21 +1,18 @@
 package processcommand
 
 import model.Command
-import model.Sha1Hex
-import utils.extractZlibDecompressedContent
+import utils.Constants
 import utils.toFullPath
 import utils.zlibDecompress
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
-import kotlin.io.path.absolute
 
 fun processCatFileCommand(command: Command.CatFile) {
     val path = command.options.sha.toFullPath()
     val rawBytes = Files.readAllBytes(path)
-    val decompressedContent = rawBytes
+    val content = rawBytes
         .zlibDecompress()
         .bytes
-        .toString(StandardCharsets.UTF_8)
-    val content = decompressedContent.extractZlibDecompressedContent()
-    println(content)
+        .decodeToString()
+        .substringAfter(Constants.NULL_BYTE)
+    print(content)
 }
